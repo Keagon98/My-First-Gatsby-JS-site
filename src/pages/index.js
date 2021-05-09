25ffb5977606e916c1dea8f6b23b8f5124394028
Keagon98 <keaggzb20v@gmail.com>
2021-05-09 19:06:09 +0200

    Added styling components

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ export default function Home() {
   return (
     <div style={{ color: `#222` }}>
       <Link to="/contact">Contact</Link>
+      <Link to="/about-css-modules">About CSS modules</Link>
       <Header headerText="Hello Gatsby!" />
       <p>What a world.</p>
       <img src="https://source.unsplash.com/random/400x200" alt="" />
