02c0234456efc87d6c5d1f415e1525171e3643cf
Keagon98 <keaggzb20v@gmail.com>
2021-05-10 00:51:15 +0200

    Added layout component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,13 @@
 import React from "react"
-import { Link } from "gatsby"
 import Header from "../components/header"
+import Layout from "../components/layout"
 
 export default function Home() {
   return (
-    <div style={{ color: `#222` }}>
-      <Link to="/contact">Contact</Link>
-      <Link to="/about-css-modules">About CSS modules</Link>
+    <Layout>
       <Header headerText="Hello Gatsby!" />
       <p>What a world.</p>
       <img src="https://source.unsplash.com/random/400x200" alt="" />
-    </div>
-  );
+    </Layout>
+  )
 }
